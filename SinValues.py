ae6377e3cbfdb614c7c5be14f73f5584d997c6af
SamAlv118 <sabralva@ucsc.edu>
2025-10-03 17:37:54 -0700

    Add files via upload
    1.5

---
+++ SinValues.py
@@ -0,0 +1,17 @@
+import numpy as np
+from astropy.table import Table
+from math import pi
+
+def main():
+        x = 0
+        xList = []
+        sinList = []
+        while x < (2*pi):
+                xList.append(x)
+                sinList.append(np.sin(x))
+                x += (2*pi)/1000
+        data = Table([xList,sinList] , names = ["x", "sin(x)"])
+        print(data)
+
+if __name__ == '__main__':
+        main()
