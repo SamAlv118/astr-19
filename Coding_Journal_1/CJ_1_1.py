3ac6b689f4a456800b89851558a32288e9dab08a
SamAlv118 <sabralva@ucsc.edu>
2025-10-04 00:12:41 -0700

    Update CJ_1_1.py
    I had written honey coconut shrimp but I meant honey WALNUT shrimp. Basically the same thing but it's more common to say honey walnut shrimp instead.

--- Coding_Journal_1/CJ_1_1.py
+++ Coding_Journal_1/CJ_1_1.py
@@ -1,7 +1,7 @@
 def AboutMe():
 	print("Name: Samuel Alvarez")
 	print("Pronouns: He/Him")
-	print("Notes: 'My favorite food is honey coconut shrimp but it is difficult to make properly so I get sad when I try to make it at home.")
+	print("Notes: 'My favorite food is honey walnut shrimp but it is difficult to make properly so I get sad when I try to make it at home.")
 	print("My favorite movie varies by genre but as of recently, Idiocracy has risen to become my favorite as I feel it reflects the path of society.'")
 
 def main():
@@ -9,3 +9,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
